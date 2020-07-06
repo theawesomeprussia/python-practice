e4964b9d68008fe2d5e28b9b45f4a3a395a5f267
Shafay <shafayumair@gmail.com>
2020-07-06 21:33:20 +0000

    started if statements

---
+++ constants-and-varliables.py
@@ -0,0 +1,30 @@
+# constants and variables 
+
+print('Hello')
+print("Hello")
+print('ur mom said" do the dishes"')
+
+print("The value of x is " +  str(5+10))
+
+cat = 22
+dog = 15
+print(str(cat-dog))
+
+pl = "Hi"
+print(pl)
+
+s = 5.5 // 2
+print(s)
+
+print(5%2)
+
+print(3**3)
+
+n = int(5.5)
+print(str(n))
+
+f = float(2)
+print(str(f))
+
+name = input("enter name: ")
+print(name)
