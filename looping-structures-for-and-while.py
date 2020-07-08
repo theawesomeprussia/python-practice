0a2c6283604c71d59e40cf7b22b3823404c1f11f
Shafay <shafayumair@gmail.com>
2020-07-08 21:36:31 +0000

    loooooop

---
+++ looping-structures-for-and-while.py
@@ -0,0 +1,27 @@
+#looping structures for and while
+
+count = 1
+while count < 10:
+  print(str(count))
+  count = count + 1
+
+x = 2
+while x < 11:
+  print(str(x))
+  x = x + 2
+
+y = 1
+while y <= 5:
+  print("lemon")
+  y = y+1
+
+word = input("Which word????  ")
+times = int(input("How many times?"))
+
+y = 1
+while y <= times:
+  print(word)
+  y = y+1
+  
+for count in range(0, 5, 1):
+  print(str(count))
