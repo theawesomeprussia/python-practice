f2ab66ab8901ceaffff4753329598c6c65649dd2
Shafay <shafayumair@gmail.com>
2020-07-20 23:42:09 +0000

    functions and new fish game

---
+++ functions.py
@@ -0,0 +1,16 @@
+#Functions
+
+def f(x):
+  answer = ((x**3 - 4)/(5*x)) + 7
+  return answer
+
+putin = int(input("What do you want to put into f(x)? "))
+print(f(putin))
+
+
+def f(h):
+  solution = ((h**7 + 6)/(7*h))
+  return solution
+
+ans = int(input("What do you want to put into f(h)? "))
+print(f(ans))
--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-#Functions
+print("Congratulations! You have been selected to start a delecious new LIVE TUNA FISH FARM! This is a privilage because there are only a few locations that can hold this fish.")
+print("How many do you want to start out with?")
+fish = int(input("enter fish 1-5 or more: "))
+if fish == 1: 
+  print("ok it would be better if u got more")
+  
+if fish == 2:
+  print("ok boomer")
 
-def f(x):
-  answer = ((x**3 - 4)/(5*x)) + 7
-  return answer
+if fish == 3:
+ print("MOOOOOOORE")
 
-putin = int(input("What do you want to put into f(x)? "))
-print(f(putin))
+if fish == 4:
+ print("MOOOOOOOOOOOOORE")
 
+if fish == 5:
+ print("meh")
 
-def f(h):
-  solution = ((h**7 + 6)/(7*h))
-  return solution
+if fish > 5 and fish < 15:
+  print("oh yeah baby oh shriplllllle")
+  
+if fish > 15:
+  print("THATS TOO MANY BOI'S")
+  fish = int(input("enter fish 1-5 or more: "))
+  if fish == 1: 
+    print("ok it would be better if u got more")
+  
+if fish == 2:
+  print("ok boomer")
 
-ans = int(input("What do you want to put into f(h)? "))
-print(f(ans))
+if fish == 3:
+ print("MOOOOOOORE")
+
+if fish == 4:
+ print("MOOOOOOOOOOOOORE")
+
+if fish == 5:
+ print("meh")
+
+if fish > 5 and fish < 15:
+  print("oh yeah baby oh shriplllllle")
+  
+if fish > 15:
+  print("u know what this is your problem now")
+
+print("ok now that u have your big bois u should get some bait fish for food, ")
+
+print("you can't have more than 40 fish but you need at least 25 to feed ur tuna")
+
+if bfish > 25:
+  print("stawp")
